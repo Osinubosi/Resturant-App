3dda46f3dddf805001403f49d2b4ff2448ba9e2d
Olayinka Yusuf <ekoballs@hotmail.com>
2021-02-03 09:49:09 +0100

    Add project files.

---
+++ Resturant App/Resturant App/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Resturant_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Device.SetFlags(new[] { "Shape Experimental", "Brush Experimental" });
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
